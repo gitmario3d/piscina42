aeeb660e9789cb7052e487919ebd092bc2239160
gitmario3d <140595777+gitmario3d@users.noreply.github.com>
2023-07-27 14:16:48 +0200

    Add files via upload

--- c07/ex03/ft_strjoin.c
+++ c07/ex03/ft_strjoin.c
@@ -5,102 +5,60 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: malena-b <malena-b@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/07/24 12:31:52 by malena-b          #+#    #+#             */
-/*   Updated: 2023/07/26 08:18:22 by malena-b         ###   ########.fr       */
+/*   Created: 2023/07/27 10:57:44 by malena-b          #+#    #+#             */
+/*   Updated: 2023/07/27 13:26:43 by malena-b         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include <stdlib.h>
 
-char	*ft_puts_in_arr(char *arr, int k, char *sep, char **strs)
-{
-	int	i;
-	int	j;
-
-	i = 0;
-	j = 0;
-	while (strs[i])
-	{
-		j = 0;
-		while (strs[i][j])
-		{
-			arr[k] = strs[i][j];
-			j++;
-			k++;
-		}
-		j = 0;
-		while (sep[j] != '\0' && strs[i + 1])
-		{
-			arr[k] = sep[j];
-			k++;
-			j++;
-		}
-		i++;
-	}
-	return (arr);
-}
-
 int	ft_strlen(char *str)
 {
 	int	i;
 
 	i = 0;
-	while (str[i] != '\0')
+	while (str[i])
 		i++;
 	return (i);
 }
 
-int	ft_calc_size(int size, char *sep, char **strs)
-{
-	int	size_sep;
-	int	i;
-	int	size_strs;
-
-	i = 0;
-	size_strs = 0;
-	while (i < size)
-	{
-		size_strs += ft_strlen(strs[i]);
-		i++;
-	}
-	size_sep = ft_strlen(sep);
-	size = size - 1;
-	return (((size * size_sep) + size_strs));
-}
-
 char	*ft_strjoin(int size, char **strs, char *sep)
 {
-	char	*arr;
+	int		arr_size;
+	int		i;
 	int		j;
 	int		k;
-	int		size_total;
+	char	*arr;
 
+	i = 0;
 	j = 0;
 	k = 0;
-	size_total = ft_calc_size(size, sep, strs);
-	arr = (char *)malloc(sizeof(char) * size_total);
-	if (size == 0)
-		return (arr);
-	arr = ft_puts_in_arr(arr, k, sep, strs);
+	arr_size = 1;
+	while (j < size)
+		arr_size += ft_strlen(strs[j++]) + ft_strlen(sep);
+	arr = malloc(sizeof(char *) * arr_size);
+	j = -1;
+	while (++j < size)
+	{
+		while (strs[j][k])
+			arr[i++] = strs[j][k++];
+		k = 0;
+		while (sep[k] && j < size - 1)
+			arr[i++] = sep[k++];
+		k = 0;
+	}
+	arr[i] = '\0';
 	return (arr);
 }
-/*int main(void)
+
+/*int main (void)
 {
-	char    **strs;
-	char    *separator;
-	char    *result;
-	int     size;
-	size = 3;
+	char	**strs;
+
 	strs = (char **)malloc(3 * sizeof(char *));
-	strs[0] = (char *)malloc(sizeof(char) * 4 + 1);
-	strs[1] = (char *)malloc(sizeof(char) * 8 + 1);
-	strs[2] = (char *)malloc(sizeof(char) * 7 + 1);
-	strs[0] = "Esto";
-	strs[1] = "funciona";
-	strs[2] = "de lujo";
-	separator = "...";
-	result = ft_strjoin(size, strs, separator);
-	printf("%s\n", result);
-	free(result);
+	strs[0] = "Esta";
+	strs[1] = "vez";
+	strs[2] = "si que si";
+	printf("%s", ft_strjoin(3, strs, "..."));
 }*/
